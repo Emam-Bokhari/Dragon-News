3bcc8cd198e3b51a049b28203a98a5b2368a19ed
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2023-10-05 15:44:54 +0600

    Added Header,Marquee,Navbar in Home component

--- src/layout/MainLayout/MainLayout.jsx
+++ src/layout/MainLayout/MainLayout.jsx
@@ -1,23 +1,11 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../../SharedComponents/Navbar/Navbar";
-import Header from "../../SharedComponents/Header";
 
 const MainLayout = () => {
     return (
         <div>
 
             {/* Section */}
-            <section className="max-w-screen-xl mx-auto px-8 md:px-16 border-2 border-red-500 my-10" >
-
-                {/* Start Header */}
-                <div>
-                    <Header/>
-                </div>
-
-                {/* Start Navbar */}
-                <div>
-                    <Navbar />
-                </div>
+            <section className="max-w-screen-xl mx-auto px-8 md:px-16 border-2 border-red-500" >
 
                 {/* Start Outlet */}
                 <div className="my-10" >
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,29 @@
+import Header from "../../SharedComponents/Header";
+import Navbar from "../../SharedComponents/Navbar/Navbar";
+import Marquee from "react-fast-marquee";
 
 const Home = () => {
     return (
         <div>
-            <h2>Home</h2>
+
+            {/* Start Header */}
+            <div>
+                <Header />
+            </div>
+
+            {/* Latest News */}
+            <div className="flex my-5 " >
+                <span className="bg-[#D72050] text-base font-medium px-4 py-1 text-white" >Latest</span>
+                <Marquee>
+                    <h2 className="text-lg font-semibold text-[#403F3F]" >Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</h2>
+                </Marquee>
+            </div>
+
+            {/* Start Navbar */}
+            <div>
+                <Navbar />
+            </div>
+
         </div>
     );
 };
